0a5d23333aea42c6ccf4be92026bc3d601a5920e
farnoosh naderi <farnooshnaderi@aol.com>
2020-12-05 19:24:42 +0200

    train if claus

---
+++ train_github.py
@@ -0,0 +1,25 @@
+
+name = input('please insert your name: ')
+team = input('what is your favorite soccor team? ')
+print ('hello'  '! + your favorite team is + team)
+
+
+
+
+
+num1 = input ('please insert the first number: ')
+num2 = input ('please insert the second number: ')
+operation = input ('please insert your desire operation among: ')
+if operation == '+':
+   results = num1 + num2
+if operation == '-': 
+    result = num1 - num2
+if operation == '*':
+    result = num1 * num2
+if operation == '/':
+    result = num1 / num2
+    
+print(result)            
+
+what I learned from the youtube about if clause is scripts same as above but I couldnt run these orders and I think I made some mistakes in some perts. please correct them and send them back to me.
+Gracias!
